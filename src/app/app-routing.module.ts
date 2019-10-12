d7d35fd790016c39807e0a6a8bc168bd06196009
Han Hsuan Lin <jercymat@gmail.com>
2019-10-13 02:51:07 +0800

    feat: basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CoverComponent } from './body/cover/cover.component';
+import { AboutComponent } from './body/about/about.component';
+import { VideosComponent } from './body/videos/videos.component';
+import { ResourcesComponent } from './body/resources/resources.component';
+import { LinksComponent } from './body/links/links.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CoverComponent, pathMatch: 'full'},
+  { path: 'about', component: AboutComponent },
+  { path: 'videos', component: VideosComponent },
+  { path: 'resources', component: ResourcesComponent },
+  { path: 'links', component: LinksComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
