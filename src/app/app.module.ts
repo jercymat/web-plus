1b64304a4eaf0f68e5d6654d68d1fea189c35b13
Hensen Lin <hensen.lin@logicsolutions.com>
2019-10-13 01:50:08 +0800

    feat: created basic component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { AboutComponent } from './body/about/about.component';
+import { VideosComponent } from './body/videos/videos.component';
+import { ResourcesComponent } from './body/resources/resources.component';
+import { LinksComponent } from './body/links/links.component';
+import { CoverComponent } from './body/cover/cover.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    FooterComponent,
+    BodyComponent,
+    AboutComponent,
+    VideosComponent,
+    ResourcesComponent,
+    LinksComponent,
+    CoverComponent
   ],
   imports: [
     BrowserModule,
