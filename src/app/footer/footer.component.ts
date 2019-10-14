393b16684834c982f5760690a376d34054becca8
Han Hsuan Lin <jercymat@gmail.com>
2019-10-14 23:35:25 +0800

    feat: footer theme color transition

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subscription, Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  footerBg$: Observable<string>;
+  footerBgColor = '#8F437A';
 
-  constructor() { }
+  constructor(private router: Router) {
+    // this.footerBg$ = router.events.subscribe(val => {
+    //   console.log(val instanceof NavigationEnd);
+    //   if (val instanceof NavigationEnd) {
+    //     console.log(val);
+    //   }
+    // });
+  }
 
   ngOnInit() {
+    this.footerBg();
+  }
+
+  footerBg() {
+    const colors = {
+      '/': '#8F437A',
+      '/about': '#8F437A',
+      '/videos': '#758AB2',
+      '/resources': '#87B3BF',
+      '/links': '#D9BA74'
+    }
+    this.footerBg$ = this.router.events.pipe(
+      filter(val => val instanceof NavigationEnd),
+      map((val: NavigationEnd) => colors[val.url])
+    );
   }
 
 }
