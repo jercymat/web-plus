62359b78ec2b42b2951f809dbffe8c585c2ef073
Han Hsuan Lin <jercymat@gmail.com>
2019-10-14 23:53:46 +0800

    feat: sidebar theme color transition

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  sidebarBg$: Observable<string>;
+  sidebarBgColor = '#F6F0F3';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.sidebarBg();
+  }
+
+  sidebarBg() {
+    const colors = {
+      '/': '#F6F0F3',
+      '/about': '#F6F0F3',
+      '/videos': '#F4F6F9',
+      '/resources': '#F4F8F9',
+      '/links': '#FCF9F4'
+    }
+    this.sidebarBg$ = this.router.events.pipe(
+      filter(val => val instanceof NavigationEnd),
+      map((val: NavigationEnd) => colors[val.url])
+    );
   }
 
 }
