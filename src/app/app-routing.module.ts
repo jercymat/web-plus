0f4815f92d3c70bf7f26f1306bb86cf80aa0e4c7
Han Hsuan Lin <jercymat@gmail.com>
2019-10-15 18:37:23 +0800

    feat: second layer routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,32 @@ import { AboutComponent } from './body/about/about.component';
 import { VideosComponent } from './body/videos/videos.component';
 import { ResourcesComponent } from './body/resources/resources.component';
 import { LinksComponent } from './body/links/links.component';
+import { AboutTechComponent } from './body/about/about-tech/about-tech.component';
+import { AboutTargetComponent } from './body/about/about-target/about-target.component';
+import { AboutMemberComponent } from './body/about/about-member/about-member.component';
+import { ResModuleComponent } from './body/resources/res-module/res-module.component';
+import { ResIntroComponent } from './body/resources/res-intro/res-intro.component';
+import { ResToolComponent } from './body/resources/res-tool/res-tool.component';
+import { ResExampleComponent } from './body/resources/res-example/res-example.component';
 
 
 const routes: Routes = [
-  { path: '', component: CoverComponent, pathMatch: 'full'},
-  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: 'cover', pathMatch: 'full'},
+  { path: 'cover', component: CoverComponent },
+  { path: 'about', component: AboutComponent, children: [
+    { path: '', redirectTo: 'tech', pathMatch: 'full' },
+    { path: 'tech', component: AboutTechComponent },
+    { path: 'target', component: AboutTargetComponent },
+    { path: 'member', component: AboutMemberComponent }
+  ]},
   { path: 'videos', component: VideosComponent },
-  { path: 'resources', component: ResourcesComponent },
+  { path: 'resources', component: ResourcesComponent, children: [
+    { path: '', redirectTo: 'intro', pathMatch: 'full' },
+    { path: 'intro', component: ResIntroComponent },
+    { path: 'module', component: ResModuleComponent },
+    { path: 'tool', component: ResToolComponent },
+    { path: 'example', component: ResExampleComponent }
+  ]},
   { path: 'links', component: LinksComponent }
 ];
 
